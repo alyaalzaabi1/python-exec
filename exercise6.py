78d61114e5fd44865f2ee6356a53fe2362301155
Alya <alya.jasem@ega.rak.ae>
2025-06-17 12:06:12 +0400

    Exercise 6: Basic lists

---
+++ exercise6.py
@@ -0,0 +1,7 @@
+favorite_foods = ["Pizza", "Sushi", "Pasta", "Ice Cream", "Tacos"]
+print("favotite foods:", favorite_foods)
+print("First food:",  favorite_foods[0])
+print("Last food:",  favorite_foods[-1])
+favorite_foods.append("Burger")
+print("Updated List:", favorite_foods)
+
